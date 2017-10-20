cdd7d0140e5650a4af2882fdb98fe038a467d68d
Nicolas VANHAEZEBROUCK <nvanhaezebrouck@CASTELIS.COM>
2017-10-20 14:36:52 +0200

    ajout du support de whoops directement dans le framework

--- src/App.php
+++ src/App.php
@@ -42,14 +42,13 @@ abstract class App {
         if(!class_exists('\\Middlewares\\Whoops'))
             return ;
 
-        function exception_error_handler($severity, $message, $file, $line) {
+        set_error_handler(function($severity, $message, $file, $line){
             if (!(error_reporting() & $severity)) {
                 // This error code is not included in error_reporting
                 return;
             }
             throw new ErrorException($message, 0, $severity, $file, $line);
-        }
-        set_error_handler("exception_error_handler");
+        });
     }
 
     /**
