e4d09e25c32843d14fd642f6cac391c6b62b2631
Nicolas Vanhaezebrouck <nvanhaezebrouck@consomind.com>
2022-01-11 09:11:57 +0100

    update model class

--- src/Model.php
+++ src/Model.php
@@ -15,4 +15,46 @@ class Model {
         return Container::getInstance()->get('Bdd')->exec($sql, $data);
     }
 
+    /**
+     * @param bool $check_conn
+     * @return mixed
+     */
+    public function connect(bool $check_conn = true){
+        return Container::getInstance()->get('Bdd')->connect($check_conn);
+    }
+
+    /**
+     * @param       $sql
+     * @param array $param_sql
+     *
+     * @return array|bool|false|int|resource
+     * @throws \Exception
+     */
+    public function open($sql, array $param_sql = array()){
+        return Container::getInstance()->get('Bdd')->open($sql, $param_sql);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function beginTransaction(){
+        return Container::getInstance()->get('Bdd')->beginTransaction();
+    }
+
+    public function commit(){
+        return Container::getInstance()->get('Bdd')->commit();
+    }
+
+    public function rollback() {
+        return Container::getInstance()->get('Bdd')->rollback();
+    }
+
+    public function fetch($stmt){
+        return Container::getInstance()->get('Bdd')->fetch($stmt);
+    }
+
+    public function fetchAll($stmt){
+        return Container::getInstance()->get('Bdd')->fetchAll($stmt);
+    }
+
 }
