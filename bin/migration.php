f47e904bd0e50f4f33a74db58065ac225d2304e4
Veka <veka610@gmail.com>
2022-01-19 09:34:25 +0100

    Update migration.php

--- bin/migration.php
+++ bin/migration.php
@@ -9,9 +9,27 @@ new class($root_path) extends \VekaServer\Framework\Console {
 
     public function run($params){
 
-        $migration = new \VekaServer\Framework\Migration(
-            $this->root_path.'/src/migration/'
-            ,'framework');
+        /** si $params['source'] manquant alors afficher l'aide */
+        if(empty($params['source'])){
+            echo 'parametre source manquant'.PHP_EOL;
+            $this->show_help();
+        }
+
+        /** recuperation de la config pour les migrations */
+        $migration_config = VekaServer\Config\Config::getInstance()->get('Migration');
+
+        if(
+            !isset( $migration_config[$params['source']])
+            || empty( ($migration_config[$params['source']]['path'] ?? ''))
+            || empty( ($migration_config[$params['source']]['name'] ?? ''))
+        ){
+            throw new Exception('No config found for source='.$params['source']);
+        }
+
+        $path = $migration_config[$params['source']]['path'];
+        $name = $migration_config[$params['source']]['name'];
+
+        $migration = new \VekaServer\Framework\Migration( $path, $name);
 
         switch($params['action'] ?? ''){
 
@@ -29,15 +47,21 @@ new class($root_path) extends \VekaServer\Framework\Console {
 
             default :
                 echo 'parametre action manquant'.PHP_EOL;
-                echo 'Exemple :'.PHP_EOL;
-                echo 'php '.basename($_SERVER['SCRIPT_NAME']).' action=upgrade'.PHP_EOL;
-                echo 'liste des actions :'.PHP_EOL;
-                echo 'upgrade : fait toute les upgrade disponible'.PHP_EOL;
-                echo 'downgrade : annule la derniere upgrade seulement'.PHP_EOL;
-                echo 'init : creer la table de migration'.PHP_EOL;
+                $this->show_help();
                 break;
         }
 
+    }
+
+    public function show_help(){
+        echo 'Exemple :'.PHP_EOL;
+        echo 'php '.basename($_SERVER['SCRIPT_NAME']).' source=app action=upgrade'.PHP_EOL;
+        echo 'la liste des sources de migration est dans le fichier de config'.PHP_EOL;
+        echo 'liste des actions :'.PHP_EOL;
+        echo 'upgrade : fait toute les upgrade disponible'.PHP_EOL;
+        echo 'downgrade : annule la derniere upgrade seulement'.PHP_EOL;
+        echo 'init : creer la table de migration'.PHP_EOL;
+        die();
     }
 
 };
