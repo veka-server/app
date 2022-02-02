4febd91bc76c9d350eae30f969922962753feb5e
Veka <veka610@gmail.com>
2022-02-02 22:38:18 +0100

    Create Log.php

---
+++ src/Log.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace VekaServer\Framework;
+
+use VekaServer\Container\Container;
+
+class Log {
+
+    public static function log($level, $message, array $context = array())
+    {
+        /** @var \Psr\Log\AbstractLogger $log */
+        $log = Container::getInstance()->get('Log');
+        $log->log($level, $message, $context);
+    }
+
+    /**
+     * System is unusable.
+     *
+     * @param string  $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public static function emergency($message, array $context = array())
+    {
+        self::log(LogLevel::EMERGENCY, $message, $context);
+    }
+
+    /**
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string  $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public static function alert($message, array $context = array())
+    {
+        self::log(LogLevel::ALERT, $message, $context);
+    }
+
+    /**
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string  $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public static function critical($message, array $context = array())
+    {
+        self::log(LogLevel::CRITICAL, $message, $context);
+    }
+
+    /**
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string  $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public static function error($message, array $context = array())
+    {
+        self::log(LogLevel::ERROR, $message, $context);
+    }
+
+    /**
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string  $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public static function warning($message, array $context = array())
+    {
+        self::log(LogLevel::WARNING, $message, $context);
+    }
+
+    /**
+     * Normal but significant events.
+     *
+     * @param string  $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public static function notice($message, array $context = array())
+    {
+        self::log(LogLevel::NOTICE, $message, $context);
+    }
+
+    /**
+     * Interesting events.
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string  $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public static function info($message, array $context = array())
+    {
+        self::log(LogLevel::INFO, $message, $context);
+    }
+
+    /**
+     * Detailed debug information.
+     *
+     * @param string  $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public static function debug($message, array $context = array())
+    {
+        self::log(LogLevel::DEBUG, $message, $context);
+    }  
+}
