9073e8c234673e6d109a25b3184cde061680dc24
Veka <veka610@gmail.com>
2022-03-10 22:03:00 +0100

    Add files via upload

---
+++ src/Plugin.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace VekaServer\Framework;
+
+use VekaServer\Config\Config;
+use VekaServer\Interfaces\PluginInterface;
+
+class Plugin
+{
+    private $plugin_list = [];
+
+    private static $_instance = null;
+
+    /**
+     * Retourne une instance singleton du fichier de config
+     * @param null $path_plugin_file Chemin vers le fichier de plugin
+     * @return null|Plugin
+     */
+    public static function getInstance($path_plugin_file = null){
+
+        if( ! (self::$_instance instanceof self ) )
+            self::$_instance = new self($path_plugin_file);
+
+        return self::$_instance;
+
+    }
+
+    /**
+     * Plugin constructor.
+     * @param string $path_plugin_file Chemin vers le fichier de plugin
+     */
+    public function __construct($path_plugin_file)
+    {
+        $this->plugin_list = require_once($path_plugin_file);
+    }
+
+    public function getAllCSSFolders()
+    {
+        $list = [Config::getInstance()->get('CSS_folder')];
+        /** @var PluginInterface $plugin */
+        foreach($this->plugin_list as $plugin){
+            $list[] = $plugin::getPathCSS();
+        }
+        return $list;
+    }
+
+    public function getAllJSFolders()
+    {
+        $list = [Config::getInstance()->get('JS_folder')];
+        /** @var PluginInterface $plugin */
+        foreach($this->plugin_list as $plugin){
+            $list[] = $plugin::getPathJS();
+        }
+        return $list;
+    }
+
+    public function getAllViewFolders()
+    {
+        $list = [Config::getInstance()->get('VIEW_folder')];
+        /** @var PluginInterface $plugin */
+        foreach($this->plugin_list as $plugin){
+            $list = array_merge($plugin::getPathView(),$list);
+        }
+        return $list;
+    }
+
+}
